9b11433d7c21117375f4d3c47c6e49c2e060c3e8
mr330136 <mr330136@outlook.com>
2020-04-27 13:03:46 +0530

    Commit all

--- src/app/employees/list-employees.component.ts
+++ src/app/employees/list-employees.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormBuilder, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-list-employees',
@@ -6,10 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-employees.component.css']
 })
 export class ListEmployeesComponent implements OnInit {
+EmailForm: FormGroup;
+validationmessages={
+  'email':{
+    'required':'Email is required'
+  },
+  'confirmemail':{
+    'required':'Confirm Email is required'
+  }
+};
+formErrors={
+  'email':'',
+  'confirmemail':''
+};
+  constructor(private fb: FormBuilder) { }
+logValidationErrors(group: FormGroup=this.EmailForm)
+{
+  Object.keys(group.controls).forEach( key =>{
+    const control= group.get(key);
+    if(control instanceof FormGroup)
+    this.logValidationErrors(control);
+    else {
+      const error=this.validationmessages[key];
+      for(const errorkey in error.keys)
+      this.formErrors[key]+=error[errorkey];
+    }
 
-  constructor() { }
+  
+    
+  })
 
+}
   ngOnInit() {
+    this.EmailForm =this.fb.group(
+      {
+        email:['',Validators.required],
+        confirmemail:['',Validators.required],
+      }
+    );
+this.logValidationErrors(this.EmailForm);
   }
 
 }
